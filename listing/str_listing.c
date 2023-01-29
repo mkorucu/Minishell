dc8c1a2c26ee086e5876758acce95f49ba49d133
Buse Keklik <busekeklik23@gmail.com>
2023-01-29 22:05:21 +0300

    str2

--- listing/str_listing.c
+++ listing/str_listing.c
@@ -6,9 +6,91 @@
 /*   By: bkeklik <bkeklik@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/29 21:39:45 by bkeklik           #+#    #+#             */
-/*   Updated: 2023/01/29 21:40:15 by bkeklik          ###   ########.fr       */
+/*   Updated: 2023/01/29 22:02:10 by bkeklik          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/minishell.h"
 
+void	find_end_pos(char **str, char type)
+{
+	(*str)++;
+	while (**str)
+	{
+		if (**str == type)
+		{
+			(*str)++;
+			if (is_whitespace(**str) || is_operator(*str))
+				break ;
+			else
+				while (**str && !is_whitespace(**str) && !is_operator(*str))
+					(*str)++;
+			return ;
+		}
+		(*str)++;
+	}
+}
+
+void	without_quote_parse(char **str)
+{
+	while (**str)
+	{
+		if (is_whitespace(**str))
+			break ;
+		if (is_operator(*str))
+			break ;
+		(*str)++;
+	}
+}
+
+void	skip_whitespace(char **str, char **head)
+{
+	while (**str && is_whitespace(**str))
+		(*str)++;
+	*head = *str;
+}
+
+void	find_end_pos(char **str)
+{
+	char type;
+	if(*str == '"')
+		type = DOUBLE_QUOTE;
+	if(*str == '\'')
+		type = SINGLE_QUOTE;
+	(*str)++;
+	while (**str)
+	{
+		if (**str == type)
+		{
+			(*str)++;
+			if (is_whitespace(**str) || is_operator(*str))
+				break ;
+			else
+				while (**str && !is_whitespace(**str) && !is_operator(*str))
+					(*str)++;
+			return ;
+		}
+		(*str)++;
+	}
+}
+
+
+
+void	parse_token_string(char **str)
+{
+	int		len;
+	char	*head;
+	char	*token_str;
+
+	skip_whitespace(str, &head);
+	if (**str && (**str == DOUBLE_QUOTE || **str == SINGLE_QUOTE))
+		find_end_pos(str);
+	else
+		without_quote_parse(str);
+	len = *str - head;
+	if (len > 0)
+	{
+		token_str = ft_substr(head, 0, len);
+		token_addback(&g_ms.token, init_token(token_str, STRING), 0);
+	}
+}
